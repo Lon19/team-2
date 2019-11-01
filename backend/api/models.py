52dc63f4c9ba67e6b4d1ba2976ae41cae6527ded
Adrian Grajdeanu <grajdeanuadrian@gmail.com>
2019-11-01 20:43:43 +0000

    added initial models

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+
+
+class ChestionarMentalHealth(models.Model):
+    user_id = models.IntegerField()
+    form_name = models.CharField(max_length=100)
+    date = models.DateTimeField()
+
+
+class ChestionarAdjustments(models.Model):
+    user_id = models.IntegerField()
+    form_name = models.CharField(max_length=100)
+    date = models.DateTimeField()
+
+
+class ChestionarSelfConfidence(models.Model):
+    user_id = models.IntegerField()
+    form_name = models.CharField(max_length=100)
+    date = models.DateTimeField()
+
+
+class ChestionarOrganizationalCulture(models.Model):
+    user_id = models.IntegerField()
+    form_name = models.CharField(max_length=100)
+    date = models.DateTimeField()
+
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import authentication, permissions
+from django.contrib.auth.models import User
+
 
 # Create your views here.
+class HelloWorld(APIView):
+    def get(self, request):
+        return Response("{'hello': 'world'}")
