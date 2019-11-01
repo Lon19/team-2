95452b5db8b85d4d65304242a76112b93383841a
Alexandru Negura <alex.negura.engr@gmail.com>
2019-11-01 22:47:52 +0200

    Add initial frontend structure

---
+++ frontend/src/app/menu/menu.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import {NbMenuItem} from '@nebular/theme';
+
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.css']
+})
+export class MenuComponent implements OnInit {
+  items: NbMenuItem[] = [
+    {
+      title: 'home',
+      link: '/',
+      icon: 'home-outline'
+    },
+    {
+      title: 'dashboard',
+      link: 'dashboard'
+    }
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
