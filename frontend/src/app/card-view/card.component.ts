db38953648e3824a94c3662db92bc125a2934c64
Baciu Ionut <ionut.b096@gmail.com>
2019-11-02 06:31:31 +0000

    Add motivational messages

--- frontend/src/app/card-view/card.component.ts
+++ frontend/src/app/card-view/card.component.ts
@@ -9,17 +9,89 @@ export class CardComponentDetails {
   overallScore: string;
   description?: string;
   buttonText: string;
+  id: number;
 
-  constructor(response: Observable<string>) {
+  // The id in this constructor may be removed if the endpoints sends an identifier
+  constructor(response: Observable<string>, id: number) {
+    this.id = id;
     if (response != null) {
       this.overallScore = '5';
-      this.description = 'Description';
+      this.changeDescriptionBasedOnScoreAndId(this.id);
     } else {
       this.title = 'Section title';
       this.overallScore = '--';
       this.description = 'Could not retrieve data. Please refresh page!';
     }
     this.buttonText = 'Expand';
+  }
+
+  changeDescriptionBasedOnScoreAndId(id: number) {
+    switch (id) {
+      case 0:
+        this.changeMentalHealthMessage();
+        break;
+      case 1:
+        this.changeAdjustmentsMessage();
+        break;
+      case 2:
+        this.changeSelfConfidenceMessage();
+        break;
+      default:
+        this.changeOrganizationalMessage();
+        break;
+    }
+  }
+
+  changeMentalHealthMessage() {
+    const intScore = +this.overallScore;
+    if (intScore <= 2) {
+      this.description = 'Hey warrior, keep going! The sun will rise and we will try again!';
+    } else {
+      if (intScore >= 3) {
+        this.description = 'Good job! Keep going! Your story is not over yet!';
+      } else {
+        this.description = 'When it rains, look for rainbows! When it is dark, look for stars!';
+      }
+    }
+  }
+
+  changeSelfConfidenceMessage() {
+    const intScore = +this.overallScore;
+    if (intScore <= 2) {
+      this.description = 'I already know what giving up feels like! I want to see what happens if I do not!';
+    } else {
+      if (intScore >= 3) {
+        this.description = 'You have got what it takes, but it will take everything you got';
+      } else {
+        this.description = 'Be strong because things will get better. t may be stormy now but it never rains forever!';
+      }
+    }
+  }
+
+  changeAdjustmentsMessage() {
+    const intScore = +this.overallScore;
+    if (intScore <= 2) {
+      this.description = 'Happiness comes from some... curious adjustment in life!';
+    } else {
+      if (intScore >= 3) {
+        this.description = 'You may not know this but people like you make me happy when they are happy!';
+      } else {
+        this.description = 'Adjustment is a difficult thing in life to do but it makes you successful!';
+      }
+    }
+  }
+
+  changeOrganizationalMessage() {
+    const intScore = +this.overallScore;
+    if (intScore <= 2) {
+      this.description = 'When you feel like quitting, think why you started!';
+    } else {
+      if (intScore >= 3) {
+        this.description = 'Nothing worth having comes easy, but you are on the right track!';
+      } else {
+        this.description = 'It always seems impossible until it is done!';
+      }
+    }
   }
 }
 
@@ -29,8 +101,7 @@ export class CardComponentProvider {
   private readonly cardComponentDetails: CardComponentDetails = null;
   private endpoint = 'Insert Endpoint Link Here';
 
-  // tslint:disable-next-line:ban-types
-  constructor(apiService: ApiService, endpointId: Number) {
+  constructor(apiService: ApiService, endpointId: number) {
     switch (endpointId) {
       case 0:
         this.endpoint = '';
@@ -45,7 +116,7 @@ export class CardComponentProvider {
         this.endpoint = '';
         break;
     }
-    this.cardComponentDetails = new CardComponentDetails(apiService.getJSON(this.endpoint));
+    this.cardComponentDetails = new CardComponentDetails(apiService.getJSON(this.endpoint), endpointId);
   }
 
   public getCardComponentDetails(): CardComponentDetails {
