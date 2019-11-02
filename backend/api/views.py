f25ec306d0b0358538a76d7e6fdcb04202e7e4dc
Adrian Grajdeanu <grajdeanuadrian@gmail.com>
2019-11-02 04:27:40 +0000

    added API endpoints for multiple stuff... too tired to write a normal commit line #peopleAreStartingToNotice

--- backend/api/views.py
+++ backend/api/views.py
@@ -33,9 +33,23 @@ class ListChestionarMentalHealth(APIView):
         return Response(serializer.data)
 
 
+class GetChestionarMentalHealth(APIView):
+    def get(self, request, uid):
+        foo = ChestionarMentalHealth.objects.filter(user_id=uid)
+        serializer = ChestionarMentalHealthSerializer(foo, many=True)
+        return Response(serializer.data)
+
+
 class ListChestionarAdjustments(APIView):
     def get(self, request):
-        users = ChestionarAdjustments.objects.all()
+        foo = ChestionarAdjustments.objects.all()
+        serializer = ChestionarAdjustmentsSerializer(foo, many=True)
+        return Response(serializer.data)
+
+
+class GetChestionarAdjustments(APIView):
+    def get(self, request, uid):
+        users = ChestionarAdjustments.objects.filter(user_id=uid)
         serializer = ChestionarAdjustmentsSerializer(users, many=True)
         return Response(serializer.data)
 
@@ -47,8 +61,22 @@ class ListChestionarSelfConfidence(APIView):
         return Response(serializer.data)
 
 
+class GetChestionarSelfConfidence(APIView):
+    def get(self, request, uid):
+        users = ChestionarSelfConfidence.objects.filter(user_id=uid)
+        serializer = ChestionarSelfConfidenceSerializer(users, many=True)
+        return Response(serializer.data)
+
+
 class ListChestionarOrganizationalCulture(APIView):
     def get(self, request):
         users = ChestionarOrganizationalCulture.objects.all()
+        serializer = ChestionarOrganizationalCultureSerializer(users, many=True)
+        return Response(serializer.data)
+
+
+class GetChestionarOrganizationalCulture(APIView):
+    def get(self, request, uid):
+        users = ChestionarOrganizationalCulture.objects.filter(user_id=uid)
         serializer = ChestionarOrganizationalCultureSerializer(users, many=True)
         return Response(serializer.data)
