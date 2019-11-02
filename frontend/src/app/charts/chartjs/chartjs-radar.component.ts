be961d89f41ed224836f46ac049da68b58f2b3f4
Alexandru Negura <alex.negura.engr@gmail.com>
2019-11-02 11:09:06 +0200

    Implement spider chart

--- frontend/src/app/charts/chartjs/chartjs-radar.component.ts
+++ frontend/src/app/charts/chartjs/chartjs-radar.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnDestroy } from '@angular/core';
-import { NbThemeService, NbColorHelper } from '@nebular/theme';
+import {Component, OnDestroy} from '@angular/core';
+import {NbThemeService, NbColorHelper} from '@nebular/theme';
+import {MentalHealthService} from '../../logic/mentalHealth/mentalHealthService';
 
 @Component({
   selector: 'ngx-chartjs-radar',
   template: `
-    <chart type="radar" [data]="data" [options]="options"></chart>
+      <chart type="radar" [data]="data" [options]="options"></chart>
   `,
 })
 export class ChartjsRadarComponent implements OnDestroy {
@@ -12,49 +13,54 @@ export class ChartjsRadarComponent implements OnDestroy {
   data: {};
   themeSubscription: any;
 
-  constructor(private theme: NbThemeService) {
+  constructor(private theme: NbThemeService,
+              mentalHealthService: MentalHealthService) {
     this.themeSubscription = this.theme.getJsTheme().subscribe(config => {
+      mentalHealthService.getHealthPlan(22222222).subscribe(questionaires => {
+          const colors: any = config.variables;
+          const chartjs: any = config.variables.chartjs;
+          const lastQuestionaire = questionaires[questionaires.length - 1];
+          const lastlastQuestionaire = questionaires[questionaires.length - 2];
 
-      const colors: any = config.variables;
-      const chartjs: any = config.variables.chartjs;
+          this.data = {
+            labels: ['Depression', 'Anxiety', 'Stress'],
+            datasets: [{
+              data: [lastQuestionaire.depression, lastQuestionaire.anxiety, lastQuestionaire.stress],
+              label: 'Last questionnaire results',
+              borderColor: colors.danger,
+              backgroundColor: NbColorHelper.hexToRgbA(colors.dangerLight, 0.5),
+            }, {
+              data: [lastlastQuestionaire.depression, lastlastQuestionaire.anxiety, lastlastQuestionaire.stress],
+              label: 'Last but one questionnaire',
+              borderColor: colors.warning,
+              backgroundColor: NbColorHelper.hexToRgbA(colors.warningLight, 0.5),
+            }],
+          };
 
-      this.data = {
-        labels: ['Eating', 'Drinking', 'Sleeping', 'Designing', 'Coding', 'Cycling', 'Running'],
-        datasets: [{
-          data: [65, 59, 90, 81, 56, 55, 40],
-          label: 'Series A',
-          borderColor: colors.danger,
-          backgroundColor: NbColorHelper.hexToRgbA(colors.dangerLight, 0.5),
-        }, {
-          data: [28, 48, 40, 19, 96, 27, 100],
-          label: 'Series B',
-          borderColor: colors.warning,
-          backgroundColor: NbColorHelper.hexToRgbA(colors.warningLight, 0.5),
-        }],
-      };
-
-      this.options = {
-        responsive: true,
-        maintainAspectRatio: false,
-        scaleFontColor: 'white',
-        legend: {
-          labels: {
-            fontColor: chartjs.textColor,
-          },
-        },
-        scale: {
-          pointLabels: {
-            fontSize: 14,
-            fontColor: chartjs.textColor,
-          },
-          gridLines: {
-            color: chartjs.axisLineColor,
-          },
-          angleLines: {
-            color: chartjs.axisLineColor,
-          },
-        },
-      };
+          this.options = {
+            responsive: true,
+            maintainAspectRatio: false,
+            scaleFontColor: 'white',
+            legend: {
+              labels: {
+                fontColor: chartjs.textColor,
+              },
+            },
+            scale: {
+              pointLabels: {
+                fontSize: 14,
+                fontColor: chartjs.textColor,
+              },
+              gridLines: {
+                color: chartjs.axisLineColor,
+              },
+              angleLines: {
+                color: chartjs.axisLineColor,
+              },
+            },
+          };
+        }
+      );
     });
   }
 
